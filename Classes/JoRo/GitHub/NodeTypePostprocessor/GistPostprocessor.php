6f6adff41d19409b60075b02022a3d1f56e9ba58
ochorocho <rothjochen@gmail.com>
2017-04-23 20:10:48 +0200

    Sonar ....

--- Classes/JoRo/GitHub/NodeTypePostprocessor/GistPostprocessor.php
+++ Classes/JoRo/GitHub/NodeTypePostprocessor/GistPostprocessor.php
@@ -17,71 +17,73 @@ use Milo\Github;
  *
  * @package JoRo\GitHub\GistPostprocessor
  */
-class GistPostprocessor implements NodeTypePostprocessorInterface {
+class GistPostprocessor implements NodeTypePostprocessorInterface
+{
 
-	/**
-	 * @Flow\InjectConfiguration(path="credentials.username")
-	 * @var string
-	 */
-	protected $username;
+    /**
+     * @Flow\InjectConfiguration(path="credentials.username")
+     * @var string
+     */
+    protected $username;
 
-	/**
-	 * @Flow\InjectConfiguration(path="credentials.token")
-	 * @var string
-	 */
-	protected $token;
+    /**
+     * @Flow\InjectConfiguration(path="credentials.token")
+     * @var string
+     */
+    protected $token;
 
-	/**
-	 * Returns the processed Configuration
-	 *
-	 * @param NodeType $nodeType (uninitialized) The node type to process
-	 * @param array $configuration input configuration
-	 * @param array $options The processor options
-	 * @return void
-	 */
-	public function process(NodeType $nodeType, array &$configuration, array $options) {
+    /**
+     * Returns the processed Configuration
+     *
+     * @param NodeType $nodeType (uninitialized) The node type to process
+     * @param array $configuration input configuration
+     * @param array $options The processor options
+     * @return void
+     */
+    public function process(NodeType $nodeType, array &$configuration, array $options)
+    {
 
-		$api = new Github\Api;
-		$token = new \Milo\Github\OAuth\Token($this->token);
+        $api = new Github\Api;
+        $tokenAuth = new \Milo\Github\OAuth\Token($this->token);
 
-		$api->setToken($token);
-		$api->getToken();
+        $api->setToken($tokenAuth);
+        $api->getToken();
 
-		$user = $this->username;
+        $user = $this->username;
 
-		/*
+        /*
             GET AVAILABLE GISTS
         */
-		$response = $api->get('/users/:user/gists', [
-			'user' => $user,
-		]);
-		$gists = $api->decode($response);
+        $response = $api->get('/users/:user/gists', [
+            'user' => $user,
+        ]);
+        $gists = $api->decode($response);
 
-		$options = [];
-		$options['empty'] = ['label' => ''];
-		foreach($gists as $gist) {
-			if(empty($gist->description)) {
-				$options[$gist->id] = ['label' => 'ID ' . $gist->id];
-			} else {
-				$options[$gist->id] = ['label' => $gist->description];
-			}
-		}
+        $options = [];
+        $options['empty'] = ['label' => ''];
+        foreach ($gists as $gist) {
+            if (empty($gist->description)) {
+                $options[$gist->id] = ['label' => 'ID ' . $gist->id];
+            } else {
+                $options[$gist->id] = ['label' => $gist->description];
+            }
+        }
 
-		$propertyName = 'gist';
-		$configuration['properties'][$propertyName] = [
-			'type' => 'string',
-			'ui' => [
-				'label' => 'Select Gist to show',
-				'reloadIfChanged' => true,
-				'inspector' => [
-					'group' => 'gist',
-					'position' => 500,
-					'editor' => 'Neos.Neos/Inspector/Editors/SelectBoxEditor',
-					'editorOptions' => [
-						'values' => $options
-					],
-				],
-			],
-		];
-	}
+        $propertyName = 'gist';
+        $configuration['properties'][$propertyName] = [
+            'type' => 'string',
+            'ui' => [
+                'label' => 'Select Gist to show',
+                'reloadIfChanged' => true,
+                'inspector' => [
+                    'group' => 'gist',
+                    'position' => 500,
+                    'editor' => 'Neos.Neos/Inspector/Editors/SelectBoxEditor',
+                    'editorOptions' => [
+                        'values' => $options
+                    ],
+                ],
+            ],
+        ];
+    }
 }
